2f2522d6047fb7e858876e302c3c2ac7e5f0221e
ArchitChaturvedi <archie.4ved@gmail.com>
2025-08-03 00:15:08 -0700

    Update SQLFile.sql

--- SQLFile.sql
+++ SQLFile.sql
@@ -1,15 +1,14 @@
 SELECT COUNT(*) FROM vgsales;
 
+
 SELECT Platform, "Year", SUM("Global_Sales") AS Sales
 FROM vgsales
 GROUP BY Platform, "Year"
 ORDER BY Platform, "Year";
 
 
-
 SELECT "Year", SUM("NA_Sales") FROM vgsales AS NA_Yearly_Sales
 GROUP BY "Year"
-
 
 
 SELECT Genre, SUM("JP_Sales") AS JP_Sales_By_Genre
@@ -18,12 +17,10 @@ GROUP BY Genre
 ORDER BY JP_Sales_By_Genre DESC;
 
 
-
 SELECT Name, EU_Sales AS EU_Shooter_Game_Sales
 FROM vgsales
 WHERE Genre = 'Shooter' 
 ORDER BY "EU_Sales" DESC;
-
 
 
 SELECT "Year", Publisher, Global_Sales
@@ -36,5 +33,6 @@ FROM vgsales
 GROUP BY Platform
 HAVING COUNT(*) > 100
 ORDER BY Num_Games DESC;
+
 
 
